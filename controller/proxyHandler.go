5136f01ab79299fb9e7ae362599dd73b7decccb6
LIOU2021 <aa78789898tw@gmail.com>
2023-09-16 16:25:41 +0800

    feat handle

--- controller/proxyHandler.go
+++ controller/proxyHandler.go
@@ -12,5 +12,14 @@ func ProxyHandler1(c *gin.Context) {
 	proxyUrl, _ := url.Parse(target)
 	proxy := httputil.NewSingleHostReverseProxy(proxyUrl)
 
+	// proxy.ModifyResponse = func(resp *http.Response) error {
+	// 	// NOTE 修改响应给客户端的response, 或者用于统计响应时间
+	// 	return nil
+	// }
+
+	// proxy.ErrorHandler = func(w http.ResponseWriter, r *http.Request, err error) {
+	// 	// 當執行proxy.ModifyResponse拋錯時，就會執行proxy.ErrorHandler
+	// }
+
 	proxy.ServeHTTP(c.Writer, c.Request)
 }
